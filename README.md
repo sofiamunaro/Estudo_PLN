8c0584ef3cae2c4c8de51e9f1280300f3d47e063
sofiamunaro <sofiamunaro2@gmail.com>
2025-03-03 12:24:37 -0300

    'alteracoes'

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Estudo_PLN
-Estudo de processamento de linguagem natural
+Estudo de processamento de linguagem natural e de git para submeter as mudanças
--- tokenizacao.py
+++ tokenizacao.py
@@ -1,9 +1,15 @@
+#tokenizacao
 import requests
 from bs4 import BeautifulSoup as bs
+import spacy
 
 url = "https://pt.wikipedia.org/wiki/Processamento_de_linguagem_natural"
 response = requests.get(url)
 soup = bs(response.text, "html.parser")
 paragraphs = soup.find_all("p")
 page_text = "\n".join([p.get_text() for p in paragraphs])
-print(page_text)
+
+nlp = spacy.load("pt_core_news_sm")
+documento = nlp(page_text)
+tokens = [token.orth_ for token in documento]
+print(tokens)
