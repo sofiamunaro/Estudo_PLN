874642d04eb1beb3de5f5e6a756b007e705018e8
sofiamunaro <sofiamunaro2@gmail.com>
2025-03-04 13:29:51 -0300

    nuvem de palavras etc

--- nuvem_de_palavras.py
+++ nuvem_de_palavras.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import spacy
-import nltk #biblioeteca de pln
+import nltk
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 url = "https://pt.wikipedia.org/wiki/Processamento_de_linguagem_natural"
 response = requests.get(url)
@@ -11,9 +12,6 @@ paragraphs = soup.find_all("p")
 page_text = "\n".join([p.get_text() for p in paragraphs])
 
 nlp = spacy.load("pt_core_news_sm")
-#possibilidade de adicionar stopwords
-#nlp.Defaults.stop_words.add('dados')
-
 documento = nlp(page_text)
 
 tokens = []
@@ -23,10 +21,10 @@ for token in documento:
     elif not(token.pos_ == "VERB") and token.is_alpha and not(token.is_stop):
         tokens.append(str.lower(token.text))
 
-frequencia = nltk.FreqDist(tokens) #cria um dicionario com o token e sua frequencia
-for chave,valor in frequencia.items():
-    print(chave + ':' + str(valor))
+frequencia = nltk.FreqDist(tokens)
 
-print(frequencia.most_common(30))
-frequencia.plot(30,cumulative=False) #mostra as 30 palavras mais frequentes
+#criação da nuvem
+nuvem = WordCloud(background_color='white')
+nuvem = nuvem.generate(" ".join(tokens))
+plt.imshow(nuvem)
 plt.show()
